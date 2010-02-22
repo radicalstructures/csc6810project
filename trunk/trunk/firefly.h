f902d42f7526cd36c8cdfaafec233dec32c4127d
nick.mancuso <nick.mancuso@ef602c3e-1d7e-11df-a1b3-3914820868db>
2010-02-22 04:30:21 +0000

    Initial Upload

---
+++ trunk/trunk/firefly.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <omp.h>
+#include <math.h>
+#include <time.h>
+#include "point.h"
+
+/* 
+    Some useful Macros
+*/
+#define POP_COUNT   50
+#define MAX_GEN     50
+
+/* 
+    Our type declarations
+*/
+typedef struct _ffp
+{
+	size_t nfflies;
+	double *x_values;
+	double *y_values;
+	double *light_values;
+} ffly_population;
+
+/* 
+    This will allow for different functions to be passed in for evaluation
+*/
+typedef void (obj_func)(ffly_population*);
+
+/* 
+    This will initiate a population of fireflies
+*/
+ffly_population* 
+init_fflies(size_t ncount, point min, point max);
+
+/*
+    Use this to clean up your population
+*/
+void 
+destroy_fflies(ffly_population *pop);
+
+/*
+    Our main function
+*/
+point 
+ffa(size_t nfireflies, size_t niteration, point min, point max, obj_func f);
+
---
+++ trunk/trunk/point.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <stdlib.h>
+#include <float.h>
+
+typedef struct _p
+{
+    double x;
+    double y;
+} point;
+
+/*
+    Use this function for a random number in [0, 1)
+*/
+double my_rand(void);
+
