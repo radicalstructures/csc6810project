ede34683386ea9f139a129e941a3e109d626a673
nick.mancuso <nick.mancuso@ef602c3e-1d7e-11df-a1b3-3914820868db>
2010-03-01 04:29:39 +0000

    multiple parameters --nick

--- trunk/main.c
+++ trunk/main.c
@@ -13,19 +13,17 @@
 */
 
 
-void our_func(ffly_population *pop);
+double our_func(ffly *fly);
 
-int 
+int
 main(int argc, char **argv)
-{		
+{
     point p;
-    point min, max;
-    min.x = min.y = -5;
-    max.x = max.y = 5;
-    
+    double mins[] = { -5.0, -5.0 };
+    double maxs[] = { 5.0, 5.0 };
     char c;
     size_t pop_count = POP_COUNT, max_gen = MAX_GEN;
-    
+
     while ( (c = getopt(argc, argv, "n:g:")) != -1)
     {
         switch (c)
@@ -36,7 +34,7 @@ main(int argc, char **argv)
         case 'g':
             max_gen = atoi(optarg);
             break;
-        case '?':           
+        case '?':
             return EXIT_FAILURE;
             break;
         default:
@@ -44,30 +42,27 @@ main(int argc, char **argv)
             break;
         }
     }
-    
-    p = ffa(pop_count, max_gen, min, max, &our_func);
-    
+
+    p = ffa(pop_count, max_gen, 2, mins, maxs, &our_func);
+
     printf("Max x: %.2lf, Max y %.2lf\n", p.x, p.y);
-    
-	return EXIT_SUCCESS;
+
+    return EXIT_SUCCESS;
 };
 
-void
-our_func(ffly_population *pop)
-{
-    unsigned register int i = 0;
+double
+our_func(ffly *fly)
+{   
     double x, y, z;
-    for (i = 0; i < pop->nfflies; i++)
-    {
-        x = pop->x_values[i];
-        y = pop->y_values[i];
-        
-        z = exp(-((x - 4) * (x - 4)) - ((y - 4) * (y - 4))) +  
-            exp(-((x + 4) * (x + 4)) - ((y - 4) * (y - 4))) + 
-            (2 * (exp(-(x*x) - (y * y)) + exp(-(x*x) - ((y+4) * (y+4)) )));
-        
-        pop->light_values[i] = z;
-    }
+    
+    x = fly->params[0];
+    y = fly->params[1];
+
+    z = exp(-((x - 4) * (x - 4)) - ((y - 4) * (y - 4))) +
+        exp(-((x + 4) * (x + 4)) - ((y - 4) * (y - 4))) +
+        (2 * (exp(-(x*x) - (y * y)) + exp(-(x*x) - ((y+4) * (y+4)) )));
+
+    return z;
 };
 
 
