6f363f0fa17ec783376bad05c9eadbc7304eb7e6
nick.mancuso <nick.mancuso@ef602c3e-1d7e-11df-a1b3-3914820868db>
2010-03-18 07:34:13 +0000

    style crap... -nick

--- main.c
+++ main.c
@@ -31,16 +31,16 @@ main(int argc, char **argv)
 {
     double *mins;
     double *maxs;
-	double min=0.0, max=0.0;
+    double min=0.0, max=0.0;
     size_t pop_count = POP_COUNT, max_gen = MAX_GEN;
     size_t i = 0, dimension = 2;
     size_t nffa = 0, nffasa = 0;
-	
-	init_params(argc, argv, &pop_count, &max_gen, &dimension, &min, &max);
 
-	//this should change to be some parameter soon...
-	obj_func func = &akley;
-    
+    init_params(argc, argv, &pop_count, &max_gen, &dimension, &min, &max);
+
+    //this should change to be some parameter soon...
+    obj_func func = &akley;
+
     mins = (double*)calloc(dimension, sizeof(double));
     maxs = (double*)calloc(dimension, sizeof(double));
 
@@ -49,40 +49,40 @@ main(int argc, char **argv)
         mins[i] = min;
         maxs[i] = max;
     }
-	
-	printf("Beginning standard firefly algorithm...\n");
+
+    printf("Beginning standard firefly algorithm...\n");
     nffa   = test_ffa(pop_count, dimension, mins, maxs, func);
 
-	printf("Evaluations necessary to be within epsilon of optima: %ld\n\n", nffa);
-	
-	printf("Beginning firefly algorithm with simulated annealing...\n");
-	nffasa = test_ffasa(pop_count, dimension, mins, maxs, func);
+    printf("Evaluations necessary to be within epsilon of optima: %ld\n\n", nffa);
 
-	printf("Evaluations necessary to be within epsilon of optima: %ld\n\n", nffasa);    
-	
+    printf("Beginning firefly algorithm with simulated annealing...\n");
+    nffasa = test_ffasa(pop_count, dimension, mins, maxs, func);
+
+    printf("Evaluations necessary to be within epsilon of optima: %ld\n\n", nffasa);
+
     free(mins);
     free(maxs);
-	
+
     return EXIT_SUCCESS;
 };
 
-void 
+void
 init_params(int argc, char **argv, size_t *npop, size_t *ngen, size_t *ndim, double *min, double *max)
 {
-	char c;
-	
-	while ( (c = getopt(argc, argv, "n:g:d:m:x:")) != -1)
+    char c;
+
+    while ( (c = getopt(argc, argv, "n:g:d:m:x:")) != -1)
     {
         switch (c)
         {
         case 'n':
-            *npop = atoi(optarg);            
+            *npop = atoi(optarg);
             break;
         case 'g':
-            *ngen = atoi(optarg);            
+            *ngen = atoi(optarg);
             break;
         case 'd':
-            *ndim = atoi(optarg);            
+            *ndim = atoi(optarg);
             break;
         case 'm':
             *min = atof(optarg);
@@ -102,9 +102,9 @@ init_params(int argc, char **argv, size_t *npop, size_t *ngen, size_t *ndim, dou
 
 double
 yang(const ffly *fly, const size_t nparams)
-{   
+{
     double x, y, z;
-    
+
     x = fly->params[0];
     y = fly->params[1];
 
@@ -123,7 +123,7 @@ akley(const ffly *fly, const size_t nparams)
     unsigned int i = 0;
     double exp1, exp2, frac;
     double sumsq = 0.0, sumcos = 0.0;
-    
+
     for (i = 0; i < nparams; i++)
     {
         sumsq += fly->params[i] * fly->params[i];
@@ -132,25 +132,25 @@ akley(const ffly *fly, const size_t nparams)
     frac = 1.0 / ((double) nparams);
     exp1 = exp(-0.2 * sqrt(frac * sumsq));
     exp2 = exp(frac * sumcos);
-    
+
     return (-20 * exp1 - exp2 + 20 + E);
 };
-        
+
 double
 schwefel(const ffly *fly, const size_t nparams)
 {
     unsigned int i = 0;
     const double a = 418.9829;
     double sum = 0.0;
-    
+
     for (i = 0; i < nparams; i++)
     {
         sum +=  fly->params[i] * sin(sqrt(abs(fly->params[i])));
     }
-    
+
     return (a * ((double)nparams) * sum);
 };
-        
+
 double
 rosenbrock(const ffly *fly, const size_t nparams)
 {
@@ -161,46 +161,46 @@ rosenbrock(const ffly *fly, const size_t nparams)
     for (i=0; i < nparams-1; i++)
     {
         x = fly->params[i];
-        
+
         part = (fly->params[i+1] - (x * x));
         part *= part;
         sum += (100 * part) + ( (1 - x) * (1 - x) );
     }
     return sum;
 };
-        
+
 double
 michalewicz(const ffly *fly, const size_t nparams)
 {
-	size_t i = 0, j = 0;
-	const size_t m = 10;
-	double x_i = 0.0;
-	double sum = 0.0;
-	double partial = 0.0;
-	
-	for (i = 0; i < nparams; i++)
-	{
-		x_i = fly->params[i];
-		partial = sin((i * (x_i * x_i)) / PI);
-		
-		for (j = 0; j < 2*m; j++)
-		{
-			partial *= partial;
-		}
-	    sum += sin(x_i) * partial;
-	}
-	return -sum;
+    size_t i = 0, j = 0;
+    const size_t m = 10;
+    double x_i = 0.0;
+    double sum = 0.0;
+    double partial = 0.0;
+
+    for (i = 0; i < nparams; i++)
+    {
+        x_i = fly->params[i];
+        partial = sin((i * (x_i * x_i)) / PI);
+
+        for (j = 0; j < 2*m; j++)
+        {
+            partial *= partial;
+        }
+        sum += sin(x_i) * partial;
+    }
+    return -sum;
 };
 
 double
 easom(const ffly *fly, const size_t nparams)
 {
 
-	double x1 = fly->params[0];
-	double x2 = fly->params[1];
+    double x1 = fly->params[0];
+    double x2 = fly->params[1];
 
-	double p1 = (x1-PI) * (x1-PI);
-	double p2 = (x2-PI) * (x2-PI);
-	
-	return -cos(x1) * cos(x2) * exp(-p1 - p2);
+    double p1 = (x1-PI) * (x1-PI);
+    double p2 = (x2-PI) * (x2-PI);
+
+    return -cos(x1) * cos(x2) * exp(-p1 - p2);
 };
