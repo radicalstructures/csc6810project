37906dbe24f935988e33004167d7ffe268601406
nick.mancuso <nick.mancuso@ef602c3e-1d7e-11df-a1b3-3914820868db>
2010-05-03 19:20:54 +0000

    cleaned up stuff before final handin. add more obj functions to test suite later -nick

--- trunk/main.c
+++ trunk/main.c
@@ -6,26 +6,14 @@
  */
 
 #include <unistd.h>
-#include "firefly.h"
-
-#define PI 3.14159265
-#define E 2.71828183
+#include "functions.h"
 
 #define POP_COUNT 50
 #define MAX_GEN 50
 
-/*
-    Our objective function declarations
-*/
-double yang(const ffly *fly, const size_t nparams);
-double akley(const ffly *fly, const size_t nparams);
-double schwefel(const ffly *fly, const size_t nparams);
-double rosenbrock(const ffly *fly, const size_t nparams);
-double michalewicz(const ffly *fly, const size_t nparams);
-double easom(const ffly *fly, const size_t nparams);
-double dejung(const ffly *fly, const size_t nparams);
 
 void init_params(int argc, char **argv, size_t *npop, size_t *ngen, size_t *ndim, double *min, double *max);
+void print_usage(void);
 
 int
 main(int argc, char **argv)
@@ -73,13 +61,8 @@ init_params(int argc, char **argv, size_t *npop, size_t *ngen, size_t *ndim, dou
 {
     char c;
 
-    if (argc < 8)
-    {
-    	printf("FFlies usage: -n NumberOfFlies [-g NumberOfGenerations] -d NumberOfDimension -m Min -x Max\n");
-	exit(EXIT_FAILURE);
-    }
     
-    while ( (c = getopt(argc, argv, "n:g:d:m:x:")) != -1)
+    while ( (c = getopt(argc, argv, "n:g:d:m:x:h")) != -1)
     {
         switch (c)
         {
@@ -98,126 +81,26 @@ init_params(int argc, char **argv, size_t *npop, size_t *ngen, size_t *ndim, dou
         case 'x':
             *max = atof(optarg);
             break;
+		case 'h':
+			print_usage();
+			print_functions();
         case '?':
-            printf("FFlies usage: -n NumberOfFlies -g NumberOfGenerations -d NumberOfDimension -m Min -x Max\n");
-            break;
+            print_usage();
+			break;
         default:
-            printf("FFlies usage: -n NumberOfFlies -g NumberOfGenerations -d NumberOfDimension -m Min -x Max\n");
+			print_usage();
             break;
         }
     }
-};
 
-double
-yang(const ffly *fly, const size_t nparams)
-{
-    double x, y, z;
-
-    x = fly->params[0];
-    y = fly->params[1];
-
-    z = exp(-((x - 4) * (x - 4)) - ((y - 4) * (y - 4))) +
-        exp(-((x + 4) * (x + 4)) - ((y - 4) * (y - 4))) +
-        (2 * (exp(-(x*x) - (y * y)) + exp(-(x*x) - ((y+4) * (y+4)) )));
-
-    return -z;
-};
-
-
-
-double
-akley(const ffly *fly, const size_t nparams)
-{
-    unsigned int i = 0;
-    double exp1, exp2, frac;
-    double sumsq = 0.0, sumcos = 0.0;
-
-    for (i = 0; i < nparams; i++)
+    if (argc < 8)
     {
-        sumsq += pow(fly->params[i], 2.0);
-        sumcos += cos(2 * PI * fly->params[i]);
+		exit(EXIT_FAILURE);
     }
-    frac = 1.0 / ((double) nparams);
-    exp1 = exp(-0.2 * sqrt(frac * sumsq));
-    exp2 = exp(frac * sumcos);
-
-    return (-20 * exp1 - exp2 + 20 + E);
 };
 
-double
-schwefel(const ffly *fly, const size_t nparams)
+void
+print_usage(void)
 {
-    unsigned int i = 0;
-    const double a = 418.9829;
-    double sum = 0.0;
-
-    for (i = 0; i < nparams; i++)
-    {
-        sum +=  -fly->params[i] * sin(sqrt(fabs(fly->params[i])));
-    }
-
-    return (a * ((double)nparams)) + sum;
-};
-
-double
-rosenbrock(const ffly *fly, const size_t nparams)
-{
-    unsigned int i = 0;
-    double sum = 0.0;
-    double part = 0.0;
-    double x;
-    for (i=0; i < nparams-1; i++)
-    {
-        x = fly->params[i];
-
-        part = (fly->params[i+1] - (x * x));
-        part *= part;
-        sum += (100 * part) + ( (1 - x) * (1 - x) );
-    }
-    return sum;
-};
-
-double
-michalewicz(const ffly *fly, const size_t nparams)
-{
-    size_t i = 0;
-    const size_t m = 10;
-    double x_i = 0.0;
-    double sum = 0.0;
-    double partial = 0.0;
-
-    for (i = 0; i < nparams; i++)
-    {
-        x_i = fly->params[i];
-        partial = sin((i * (x_i * x_i)) / PI);
-
-        sum += sin(x_i) * pow(partial, 2 * m);
-    }
-    return -sum;
-};
-
-double
-easom(const ffly *fly, const size_t nparams)
-{
-
-    double x1 = fly->params[0];
-    double x2 = fly->params[1];
-
-    double p1 = (x1-PI) * (x1-PI);
-    double p2 = (x2-PI) * (x2-PI);
-
-    return -cos(x1) * cos(x2) * exp(-p1 - p2);
-};
-
-double
-dejung(const ffly *fly, const size_t nparams)
-{
-	size_t i = 0;
-	double sum = 0.0;
-
-	for (i = 0; i < nparams; i++)
-	{
-		sum += fly->params[i] * fly->params[i];
-	}
-	return sum;
-};
+   	printf("FFlies usage: -n NumberOfFlies [-g NumberOfGenerations] -d NumberOfDimension -m Min -x Max\n");
+}
--- trunk/makefile
+++ trunk/makefile
@@ -1,18 +1,17 @@
 CFLAGS = -Wall -lm
 OPT = -O3
 CC = gcc 
+FILES = main.c functions.c firefly.c 
 
 all :
-	$(CC) $(CFLAGS) $(OPT) main.c firefly.c    -o fflies
+	$(CC) $(CFLAGS) $(OPT) $(FILES) -o fflies
 omp:
-	$(CC) $(CFLAGS) $(OPT) -fopenmp main.c firefly.c   -o fflies
+	$(CC) $(CFLAGS) $(OPT) -fopenmp $(FILES) -o fflies
 ompdebug:
-	$(CC) $(CFLAGS) -g -fopenmp main.c firefly.c   -o fflies
+	$(CC) $(CFLAGS) -g -fopenmp $(FILES) -o fflies
 debug :
-	$(CC) $(CFLAGS) -g main.c firefly.c    -o fflies
+	$(CC) $(CFLAGS) -g $(FILES) -o fflies
 test :
 	$(CC) -lm -lcunit ./tests/unittests.c -o fflytests
 clean :
-	rm fflies
-cleantest :
-	rm ./tests/fflytests
+	rm fflies ./tests/fflytests
