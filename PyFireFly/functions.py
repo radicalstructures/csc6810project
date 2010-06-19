fbe0e077c971799653190899fb2ff04c10b25f5d
nick.mancuso <nick.mancuso@ef602c3e-1d7e-11df-a1b3-3914820868db>
2010-06-19 21:53:20 +0000

    Functional Framework. Still needs more testing -Nick

---
+++ PyFireFly/functions.py
@@ -0,0 +1,22 @@
+""" Functions for PyFirefly """
+
+def DeJung(dim):
+    return ObjFunc(__dejung, [-5 for i in xrange(dim)], [5 for i in xrange(dim)])
+
+def __dejung(coords):
+    powers = [x**2 for x in coords]
+    return reduce(lambda x,y: x+y, powers)
+
+
+class ObjFunc:
+    """ This is our objective function.
+        It contains the function as well as its bounds"""
+
+    def __init__(self, func, mins, maxs):
+        self.func = func
+        self.mins = mins
+        self.maxs = maxs
+
+    def eval(self, coords):
+        """ Evaluates the function """
+        return self.func(coords)
