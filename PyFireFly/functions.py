4a8eb7bc7a7b0e45fcb42dc782dfa2dcfd736372
nick.mancuso <nick.mancuso@ef602c3e-1d7e-11df-a1b3-3914820868db>
2010-06-29 02:34:29 +0000

    added another objective function and also redefined some old ones

--- PyFireFly/functions.py
+++ PyFireFly/functions.py
@@ -2,16 +2,26 @@
 import math
 
 def DeJung(dim):
-    return ObjFunc(__dejung, [-5 for i in xrange(dim)], [5 for i in xrange(dim)])
-
-def __dejung(coords):
-    powers = [x**2 for x in coords]
-    return reduce(lambda x,y: x+y, powers)
+    """ returns the dejung objective function with 
+        mins and maxs defined """
+    return ObjFunc(_dejung, [-5.14]*dim, [5.14]*dim)
 
 def Ackley(dim):
-    return ObjFunc(__ackley, [-30 for i in xrange(dim)], [30 for i in xrange(dim)])
+    """ returns the ackley objective function with
+        mins and maxs defined """
+    return ObjFunc(_ackley, [-30.0]*dim, [30.0]*dim)
 
-def __ackley(coords):
+def Rastrigin(dim):
+    """ returns the Rastrigin objective function with
+        mins and maxs defined """
+    return ObjFunc(_rastrigin, [-5.0]*dim, [5.0]*dim)
+
+def _dejung(coords):
+    """ the sphere (dejung) function """
+    return sum([x**2 for x in coords])
+
+def _ackley(coords):
+    """ the ackley function """
     n = len(coords)
     a = 20
     b = 0.2
@@ -19,11 +29,15 @@ def __ackley(coords):
     s1 = s2 = 0.0
 
     for i in coords:
-        s1 += i**2;
+        s1 += i**2
         s2 += math.cos(c*i)
 
     return -a * math.exp(-b * math.sqrt((1.0/n) * s1)) - \
             math.exp((1.0/n) * s2) + a + math.exp(1)
+
+def _rastrigin(coords):
+    """ rastrigin function """
+    return 20 + sum([x**2 - 10.0*math.cos(2.0 * math.pi * x) for x in coords])
 
 class ObjFunc:
     """ This is our objective function.
