0c9c17fbad661098d1ee8f60a5de381bd50668fd
nick.mancuso <nick.mancuso@ef602c3e-1d7e-11df-a1b3-3914820868db>
2010-10-28 06:24:03 +0000

    lots of bugfixes, and a few functionality additions

--- trunk/PyFireFly/pso.py
+++ trunk/PyFireFly/pso.py
@@ -3,19 +3,12 @@
 '''
 import numpy as np
 from math import fabs
-from functions import *
+from functions import Function
 from BIP.Bayes.lhs import lhs
 from scipy.stats import uniform
 
 class PSO(object):
     
-    _funcs = { 'sphere' : Sphere ,
-            'ackley' : Ackley,
-            'michalewicz' : Michalewicz,
-            'rosenbrock' : Rosenbrock,
-            'rastrigin' : Rastrigin,
-            'easom' : Easom}
-
     def __init__(self, gen, size, alpha, beta):
         self.gen = int(gen)
         self.size = int(size)
@@ -23,24 +16,33 @@ class PSO(object):
         self.beta = float(beta)
         self.best = None
 
-    def generate_pop(self, size, func):
+    def _generate_pop(self, psize, func):
         ''' initializes our population 
         '''
         
+        mins = np.array(func.mins)
+        maxs = np.array(func.maxs)
+        lows = -np.abs(maxs - mins)
+        hihs = np.abs(maxs - mins)
+        
+        # calculate our initial positions
         dim = len(func.maxs)
-        params = [(func.mins[i], func.maxs[i] - func.mins[i]) for i in xrange(dim)]
-        seeds = np.array(lhs([uniform]*dim, params, size, True, np.identity(dim))).T
+        params = [(mins[i], maxs[i] - mins[i]) for i in xrange(dim)]
+        seeds = np.array(lhs([uniform]*dim, params, psize, True, np.identity(dim))).T
         seeds.astype(np.float32)
 
-        return [Particle(func, np.array(seeds[i])) for i in xrange(size)]
+        # calculate our initial velocities
+        v_seeds = np.array([uniform.rvs(lows[i], hihs[i], size=psize) for i in range(dim)]).T
+
+        return [Particle(func, np.array(seeds[i]), np.array(v_seeds[i])) for i in xrange(psize)]
 
     def run(self, func_name, dim_count):
         ''' runs the simulation given the function name
             and dimension count
         '''
 
-        func = self._funcs[func_name](dim_count)
-        self.pop = self.generate_pop(self.size, func)
+        func = Function(func_name)(dim_count)
+        self.pop = self._generate_pop(self.size, func)
         self.best = min(self.pop)
 
         for _ in xrange(self.gen):
@@ -56,35 +58,34 @@ class PSO(object):
         '''
 
         bests = []
-        func = self._funcs[func_name](dim_count)
-        self.pop = self.generate_pop(self.size, func)
-        self.best = min(self.pop)
+        func = Function(func_name)(dim_count)
+        self.pop  = self._generate_pop(self.size, func)
+        p_min     = min(self.pop)
+        self.best = np.copy(p_min.position)
+        self.best_val = p_min.val
 
+        bests.append(self.best_val)
         for _ in xrange(self.gen):
-            self.pop = [p.map_eval(self.best.position, self.alpha, self.beta) for p in self.pop]
-            self.best = min(self.pop)
-            bests.append(self.best.val)
+            self.pop  = [p.map_eval(self.best, self.alpha, self.beta) for p in self.pop]
+            p_min     = min(self.pop)
+
+            if p_min.val < self.best_val:
+                self.best = np.copy(p_min.position)
+                self.best_val = p_min.val
+
+            bests.append(self.best_val)
 
         return np.array(bests)
 
-    def _generate_pop(self, size, func):
-        ''' initializes our population 
-        '''
-        
-        dim = len(func.maxs)
-        params = [(func.mins[i], func.maxs[i] - func.mins[i]) for i in xrange(dim)]
-        seeds = np.array(lhs([uniform]*dim, params, size, True, np.identity(dim))).T
-        seeds.astype(np.float32)
-        
 class Particle(object):
 
-    def __init__(self, func, seeds):
+    def __init__(self, func, seeds, v_seeds):
         self.func = func
-        self.position = np.array(seeds)
-        self.velocity = np.array(seeds)
-        self.best = np.array(seeds)
-        self.val = np.inf
-    
+        self.position = np.copy(np.array(seeds))
+        self.velocity = np.copy(np.array(v_seeds))
+        self.best     = np.copy(np.array(seeds))
+        self.eval()
+
     def __cmp__(self, particle):
         if isinstance(particle, Particle):
             return cmp(self.val, particle.val)
@@ -109,17 +110,20 @@ class Particle(object):
         self.eval()
 
         if self.val < tmp:
-            self.best = np.array(self.position)
+            self.best = np.copy(self.position)
 
         return self
 
     def move(self, global_best, alpha, beta):
         for i, coord in enumerate(self.velocity):
-            tval = coord + ((alpha*(uniform.rvs()))*(global_best[i] - self.position[i])) + \
-                    ((beta * uniform.rvs()) * (self.best[i] - self.position[i]))
+            t_min = self.func.mins[i] * 0.5
+            t_max = self.func.maxs[i] * 0.5
+
+            tval = coord + ((alpha*uniform.rvs())*(self.best[i] - self.position[i])) + \
+                    ((beta * uniform.rvs()) * (global_best[i] - self.position[i]))
             
-            self.velocity[i] = self.func.mins[i] if tval < self.func.mins[i] \
-                    else self.func.maxs[i] if tval > self.func.maxs[i] else tval
+            self.velocity[i] = t_min if tval < t_min \
+                    else t_max if tval > t_max else tval
 
         for i, coord in enumerate(self.position):
             tval = coord + self.velocity[i]
--- trunk/PyFireFly/run.py
+++ trunk/PyFireFly/run.py
@@ -1,10 +1,9 @@
 from firefly import *
 from pso import *
-from sys import argv
-from math import fabs
+from sima import *
 import numpy as np
 
-def test(func_name, dimension_count, iteration_count, sample_count):
+def test(func_name, dimension_count, iteration_count, sample_count, cpu_count):
     ''' this function will run all metaheurstic algorithms against
         the given function for the specified amount of samples.
         the mean value will be output in a file for each algorithm
@@ -15,7 +14,7 @@ def test(func_name, dimension_count, iteration_count, sample_count):
     f_b = Population(iteration_count, 40, 0.5, 1.0, 1.0)
     f_c = Population(iteration_count, 40, 1.0, 1.0, 1.0)
     pso = PSO(iteration_count, 40, 2.0, 2.0)
-    sma = SA(100, 0.01)
+    sma = SA(1000, 0.01)
     samples = [[],[],[],[],[]]
 
     c = lambda a: np.mean(np.array(a), axis=0)
@@ -23,24 +22,25 @@ def test(func_name, dimension_count, iteration_count, sample_count):
     with open('./data/' + func_name + '.dat', 'w') as file:
         file.write('iters   fa  faboltz facauchy    pso sa\n')
         for _ in range(sample_count):
-            samples[0].append(f_n.iter_test(func_name, dimension_count, Population.NONE, 1))
-            samples[1].append(f_b.iter_test(func_name, dimension_count, Population.BOLTZMANN, 1))
-            samples[2].append(f_c.iter_test(func_name, dimension_count, Population.CAUCHY, 1))
+            samples[0].append(f_n.iter_test(func_name, dimension_count, Population.NONE, cpu_count))
+            samples[1].append(f_b.iter_test(func_name, dimension_count, Population.BOLTZMANN, cpu_count))
+            samples[2].append(f_c.iter_test(func_name, dimension_count, Population.CAUCHY, cpu_count))
             samples[3].append(pso.iter_test(func_name, dimension_count))
-            samples[4].append(sma.iter_test(func_name, dimension_count, iteration_count))
+            samples[4].append(sma.iter_test(func_name, dimension_count, iteration_count, SA.CAUCHY))
 
-        final = np.array([np.arange(iteration_count)] + [c(data) for data in samples])
+        # we add 1 to count for the initial case
+        final = np.array([np.arange(iteration_count+1)] + [c(data) for data in samples])
         for line in final.T:
             line.tofile(file, sep='\t')
             file.write('\n')
 
 def experiment():
-    test('sphere', 32, 40, 2)
-    #test('ackley', 128, 2, 100)
-    #test('michalewicz', 16, 2, 100)
-    #test('rosenbrock', 16, 2, 100)
-    #test('rastrigin', 16, 2, 100)
-    #test('easom', 2, 2, 100)
+    #test('sphere', 32, 4, 2, 2)
+    #test('ackley', 32, 4, 2, 2)
+    #test('michalewicz', 16, 2, 2, 2)
+    test('rosenbrock', 16, 2, 2, 2)
+    #test('rastrigin', 16, 2, 2, 2)
+    #test('easom', 2, 2, 2, 2)
 
 if __name__ == '__main__':
     experiment()
