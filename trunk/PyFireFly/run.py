877b8f3c87346efe47887dd2226c92dbf338f26a
nick.mancuso <nick.mancuso@ef602c3e-1d7e-11df-a1b3-3914820868db>
2010-11-02 18:43:22 +0000

    added error handling to the run script

--- trunk/PyFireFly/run.py
+++ trunk/PyFireFly/run.py
@@ -1,6 +1,7 @@
-from firefly import *
-from pso import *
-from sima import *
+from firefly import Population
+from pso import PSO
+from sima import SA
+from functions import Function
 import numpy as np
 
 def test(func_name, dimension_count, iteration_count, sample_count, cpu_count):
@@ -10,37 +11,43 @@ def test(func_name, dimension_count, iteration_count, sample_count, cpu_count):
         for each iteration, showcasing the convergence over time
     '''
 
+    fun = Function(func_name)(dimension_count)
+    xstar = fun.eval(fun.xstar)
+
     f_n = Population(iteration_count, 40, 0.1, 1.0, 1.0)
     f_b = Population(iteration_count, 40, 0.5, 1.0, 1.0)
     f_c = Population(iteration_count, 40, 1.0, 1.0, 1.0)
     pso = PSO(iteration_count, 40, 2.0, 2.0)
     sma = SA(1000, 0.01)
-    samples = [[],[],[],[],[]]
+    samples = [[], [], [], [], []]
 
-    c = lambda a: np.mean(np.array(a), axis=0)
+    calc_mean = lambda a: np.mean(np.array(a), axis=0)
 
-    with open('./data/' + func_name + '.dat', 'w') as file:
-        file.write('iters   fa  faboltz facauchy    pso sa\n')
-        for _ in range(sample_count):
-            samples[0].append(f_n.iter_test(func_name, dimension_count, Population.NONE, cpu_count))
-            samples[1].append(f_b.iter_test(func_name, dimension_count, Population.BOLTZMANN, cpu_count))
-            samples[2].append(f_c.iter_test(func_name, dimension_count, Population.CAUCHY, cpu_count))
-            samples[3].append(pso.iter_test(func_name, dimension_count))
-            samples[4].append(sma.iter_test(func_name, dimension_count, iteration_count, SA.CAUCHY))
+    with open('./data/' + func_name + '.dat', 'w') as out_file:
+        try:
+            out_file.write('iters   xstar   fa  faboltz facauchy    pso sa\n')
+            for _ in range(sample_count):
+                samples[0].append(f_n.iter_test(func_name, dimension_count, Population.NONE, cpu_count))
+                samples[1].append(f_b.iter_test(func_name, dimension_count, Population.BOLTZMANN, cpu_count))
+                samples[2].append(f_c.iter_test(func_name, dimension_count, Population.CAUCHY, cpu_count))
+                samples[3].append(pso.iter_test(func_name, dimension_count))
+                samples[4].append(sma.iter_test(func_name, dimension_count, iteration_count, SA.CAUCHY))
 
-        # we add 1 to count for the initial case
-        final = np.array([np.arange(iteration_count+1)] + [c(data) for data in samples])
-        for line in final.T:
-            line.tofile(file, sep='\t')
-            file.write('\n')
+            # we add 1 to count for the initial case
+            final = np.array([np.arange(iteration_count+1)] + [np.array([xstar]*(iteration_count + 1))] + [calc_mean(data) for data in samples])
+            for line in final.T:
+                line.toout_file(file, sep='\t')
+                out_file.write('\n')
+        except Exception as exc:
+            out_file.write(str(exc))
 
 def experiment():
-    test('sphere', 32, 1000, 10, 2)
-    test('ackley', 32, 1000, 10, 2)
-    test('michalewicz', 16, 1000, 10, 2)
-    test('rosenbrock', 8, 1000, 10, 2)
-    test('rastrigin', 8, 1000, 10, 2)
-    test('easom', 2, 1000, 10, 2)
+    #test('sphere', 32, 1000, 10, 2)
+    #test('ackley', 32, 1000, 10, 2)
+    test('michalewicz', 16, 10, 10, 1)
+    #test('rosenbrock', 2, 2000, 10, 2)
+    #test('rastrigin', 8, 1000, 10, 2)
+    #test('easom', 2, 1000, 10, 2)
 
 if __name__ == '__main__':
     experiment()
